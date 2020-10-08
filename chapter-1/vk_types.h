1ca0344cc1775f18de47cc7c26fde900021e7b63
Victor Blanco <vblanco20-1@hotmail.com>
2020-10-08 10:13:25 +0200

    Deleting tutorial code from engine branch

--- chapter-1/vk_types.h
+++
@@ -1,8 +0,0 @@
-﻿// vulkan_guide.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <vulkan/vulkan.h>
-
-//we will add our main reusable types here
