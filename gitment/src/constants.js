f0290e466fc8c0e9d0fd92be46db94c2ac9237ae
Victor Blanco <vblanco20-1@hotmail.com>
2020-10-24 13:30:00 +0200

    Gitment is abandoned and doesnt work. Full removal

--- gitment/src/constants.js
+++
@@ -1,4 +0,0 @@
-export const LS_ACCESS_TOKEN_KEY = 'gitment-comments-token'
-export const LS_USER_KEY = 'gitment-user-info'
-
-export const NOT_INITIALIZED_ERROR = new Error('Comments Not Initialized')
