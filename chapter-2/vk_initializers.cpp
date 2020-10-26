e1e76ff85114883d67849b3dfa4d1123ac58b07a
Victor Blanco <vblanco20-1@hotmail.com>
2020-10-26 19:57:59 +0100

    Fixed flags in initializers

--- chapter-2/vk_initializers.cpp
+++ chapter-2/vk_initializers.cpp
@@ -1,6 +1,6 @@
 ﻿#include <vk_initializers.h>
 
-VkCommandPoolCreateInfo vkinit::command_pool_create_info(uint32_t queueFamilyIndex, VkCommandPoolResetFlags flags /*= 0*/)
+VkCommandPoolCreateInfo vkinit::command_pool_create_info(uint32_t queueFamilyIndex, VkCommandPoolCreateFlags flags /*= 0*/)
 {
 	VkCommandPoolCreateInfo info = {};
 	info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
--- chapter-3-scene/vk_initializers.h
+++ chapter-3-scene/vk_initializers.h
@@ -6,8 +6,7 @@
 #include <vk_types.h>
 
 namespace vkinit {
-
-	VkCommandPoolCreateInfo command_pool_create_info(uint32_t queueFamilyIndex, VkCommandPoolResetFlags flags = 0);
+	VkCommandPoolCreateInfo command_pool_create_info(uint32_t queueFamilyIndex, VkCommandPoolCreateFlags flags = 0);
 
 	VkCommandBufferAllocateInfo command_buffer_allocate_info(VkCommandPool pool, uint32_t count = 1, VkCommandBufferLevel level = VK_COMMAND_BUFFER_LEVEL_PRIMARY);
 
