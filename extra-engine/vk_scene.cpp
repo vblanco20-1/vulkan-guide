0fde8288011dfd2c06c78236aeafbd0ce87502a5
Victor Blanco <vblanco20-1@hotmail.com>
2020-11-09 15:26:25 +0100

    added the missing scene files

---
+++ extra-engine/vk_scene.cpp
@@ -0,0 +1,222 @@
+﻿#include <vk_scene.h>
+#include <vk_engine.h>
+#include "Tracy.hpp"
+
+Handle<RenderObject2> RenderScene::register_object(RenderObject* object, PassTypeFlags passes)
+{
+	RenderObject2 newObj;
+	newObj.bounds = object->bounds;
+	newObj.transformMatrix = object->transformMatrix;
+	newObj.material = getMaterialHandle(object->material);
+	newObj.meshID = getMeshHandle(object->mesh);
+	newObj.updateIndex = (uint32_t)-1;
+	newObj.customSortKey = object->customSortKey;
+	Handle<RenderObject2> handle;
+	handle.handle = renderables.size();
+	
+	renderables.push_back(newObj);
+
+	if (passes == PassTypeFlags::Forward)
+	{
+		meshPasses[0].unbatchedObjects.push_back(handle);
+	}
+
+	update_object(handle);
+	return handle;
+}
+
+void RenderScene::register_object_batch(RenderObject* first, uint32_t count, PassTypeFlags passes)
+{
+	renderables.reserve(count);
+
+	for (uint32_t i = 0; i < count; i++) {
+		register_object(&(first[i]), passes);
+	}
+}
+
+void RenderScene::update_transform(Handle<RenderObject2> objectID, const glm::mat4& localToWorld)
+{
+	get_object(objectID)->transformMatrix = localToWorld;
+	update_object(objectID);
+}
+
+
+void RenderScene::update_object(Handle<RenderObject2> objectID)
+{
+	if (get_object(objectID)->updateIndex == (uint32_t)-1)
+	{
+		get_object(objectID)->updateIndex = dirtyObjects.size();
+		dirtyObjects.push_back(objectID);
+	}
+}
+
+void RenderScene::fill_objectData(GPUObjectData* data)
+{
+	static int ntimes = 0;
+	ntimes++;
+	if (ntimes > 3) return;
+	for(int i = 0; i < renderables.size(); i++)
+	{
+		GPUObjectData object;
+		const RenderObject2 &renderable = renderables[i];
+
+		object.modelMatrix = renderable.transformMatrix;
+		object.origin_rad = glm::vec4(renderable.bounds.origin, renderable.bounds.radius);
+		object.extents = glm::vec4(renderable.bounds.extents, renderable.bounds.valid? 1.f : 0.f);
+
+		memcpy(data + i, &object, sizeof(GPUObjectData));
+	}
+}
+
+void RenderScene::fill_instanceArray(uint32_t* data)
+{
+	static int ntimes = 0;
+	ntimes++;
+	if (ntimes > 3) return;
+	for (int i = 0; i < meshPasses[0].flat_batches.size(); i++)
+	{
+		data[i] = meshPasses[0].flat_batches[i].object.handle;
+	}
+}
+
+void RenderScene::fill_indirectArray(IndirectObject* data)
+{
+	static int ntimes = 0;
+	ntimes++;
+	if (ntimes > 3) return;
+	for (int i = 0; i < meshPasses[0].flat_batches.size(); i++) {
+		RenderObject2* obj = get_object(meshPasses[0].flat_batches[i].object);
+
+		data[i].command.firstInstance = i;
+		data[i].command.instanceCount = 1;
+		data[i].command.firstIndex = 0;
+		data[i].command.vertexOffset = 0;
+		data[i].command.indexCount = get_mesh(obj->meshID)->_indices.size();
+		data[i].objectID = meshPasses[0].flat_batches[i].object.handle;
+	}
+}
+
+void RenderScene::build_batches()
+{
+	refresh_pass(&meshPasses[0]);
+}
+
+void RenderScene::refresh_pass(MeshPass* pass)
+{
+	{
+		pass->flat_batches.clear();
+		ZoneScopedNC("Fill DrawList", tracy::Color::Blue2);
+		for (int i = 0; i < pass->unbatchedObjects.size(); i++) {
+			RenderObject2* object = get_object(pass->unbatchedObjects[i]);
+		
+			{
+				RenderScene::RenderBatch newCommand;
+
+				newCommand.object = pass->unbatchedObjects[i];
+
+				//pack mesh id and material into 32 bits
+				uint64_t meshmat = object->material.handle << 32 | object->meshID.handle;
+
+				newCommand.sortKey = meshmat;
+
+				pass->flat_batches.push_back(newCommand);
+			}
+		}
+	}
+
+	{
+		ZoneScopedNC("Draw Sort", tracy::Color::Blue1);
+		std::sort(pass->flat_batches.begin(), pass->flat_batches.end(), [](const RenderScene::RenderBatch& A, const RenderScene::RenderBatch& B) {
+			return A.sortKey < B.sortKey;
+			});
+	}
+
+	{
+		ZoneScopedNC("Draw Merge", tracy::Color::Blue);
+
+		pass->batches.clear();
+
+		RenderScene::IndirectBatch newBatch;
+		newBatch.first = 0;
+		newBatch.count = 0;
+		newBatch.material = get_object(pass->flat_batches[0].object)->material;
+		newBatch.meshID = get_object(pass->flat_batches[0].object)->meshID;
+
+		pass->batches.push_back(newBatch);
+
+		for (int i = 0; i < pass->flat_batches.size(); i++) {
+			RenderObject2* obj = get_object(pass->flat_batches[i].object);
+			RenderScene::IndirectBatch* back = &pass->batches.back();
+
+			if (obj->meshID.handle == back->meshID.handle
+				&& obj->material.handle == back->material.handle)
+			{
+				back->count++;
+				back->AABBMax = glm::max(back->AABBMax, obj->bounds.origin + obj->bounds.extents);
+				back->AABBMin = glm::min(back->AABBMin, obj->bounds.origin - obj->bounds.extents);
+			}
+			else {
+
+				newBatch.first = i;
+				newBatch.count = 1;
+				newBatch.material = obj->material;
+				newBatch.meshID = obj->meshID;
+				newBatch.AABBMax = obj->bounds.origin + obj->bounds.extents;
+				newBatch.AABBMin = obj->bounds.origin - obj->bounds.extents;
+
+				pass->batches.push_back(newBatch);
+			}
+		}
+	}
+}
+
+RenderObject2* RenderScene::get_object(Handle<RenderObject2> objectID)
+{
+	return &renderables[objectID.handle];
+}
+
+Mesh* RenderScene::get_mesh(Handle<Mesh> objectID)
+{
+	return meshes[objectID.handle];
+}
+
+Material* RenderScene::get_material(Handle<Material> objectID)
+{
+	return materials[objectID.handle];
+}
+
+Handle<Material> RenderScene::getMaterialHandle(Material* m)
+{
+	Handle<Material> handle;
+	auto it = materialConvert.find(m);
+	if (it == materialConvert.end())
+	{
+		uint32_t index = materials.size();
+		 materials.push_back(m);
+
+		 handle.handle = index;
+		 materialConvert[m] = handle;
+	}
+	else {
+		handle = (*it).second;
+	}
+	return handle;
+}
+
+Handle<Mesh> RenderScene::getMeshHandle(Mesh* m)
+{
+	Handle<Mesh> handle;
+	auto it = meshConvert.find(m);
+	if (it == meshConvert.end())
+	{
+		uint32_t index = meshes.size();
+		meshes.push_back(m);
+
+		handle.handle = index;
+		meshConvert[m] = handle;
+	}
+	else {
+		handle = (*it).second;
+	}
+	return handle;
+}
---
+++ extra-engine/vk_scene.h
@@ -0,0 +1,117 @@
+﻿// vulkan_guide.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <vk_types.h>
+#include <vk_scene.h>
+
+#include <vk_mesh.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+
+#include <array>
+#include <vector>
+#include <unordered_map>
+
+template<typename T>
+struct Handle {
+	uint32_t handle;
+};
+
+struct Material;
+struct RenderObject;
+struct Mesh;
+struct GPUObjectData;
+
+struct IndirectObject {
+	VkDrawIndexedIndirectCommand command;
+	uint32_t objectID;
+};
+enum class PassTypeFlags : uint8_t {
+	Forward = 1 << 0,
+	Prepass = 1 << 1,
+	DirectionalShadow = 1 << 2
+};
+
+struct RenderObject2 {
+
+	Handle<Mesh> meshID;
+	Handle<Material> material;
+
+	uint32_t updateIndex;
+	uint32_t customSortKey{0};
+
+	glm::mat4 transformMatrix;
+
+	RenderBounds bounds;
+};
+
+
+
+
+class RenderScene {
+public:
+	struct IndirectBatch {
+		Handle<Mesh> meshID;
+		Handle<Material> material;
+
+		uint32_t first;
+		uint32_t count;
+
+		glm::vec3 AABBMin;
+		glm::vec3 AABBMax;
+	};
+	struct RenderBatch {
+		Handle<RenderObject2> object;
+		uint64_t sortKey;
+	};
+
+	struct MeshPass {
+		std::vector<RenderScene::IndirectBatch> batches;
+
+		std::vector<Handle<RenderObject2>> unbatchedObjects;
+
+		std::vector<RenderScene::RenderBatch> flat_batches;
+	};
+
+	Handle<RenderObject2> register_object(RenderObject* object, PassTypeFlags passes);
+
+	void register_object_batch(RenderObject* first, uint32_t count, PassTypeFlags passes);
+
+	void update_transform(Handle<RenderObject2> objectID,const glm::mat4 &localToWorld);
+	void update_object(Handle<RenderObject2> objectID);
+	
+	void fill_objectData(GPUObjectData* data);
+	void fill_instanceArray(uint32_t* data);
+	void fill_indirectArray(IndirectObject* data);
+
+	void build_batches();
+
+	void refresh_pass(MeshPass* pass);
+
+	RenderObject2* get_object(Handle<RenderObject2> objectID);
+	Mesh* get_mesh(Handle<Mesh> objectID);
+	Material* get_material(Handle<Material> objectID);
+
+	std::vector<RenderObject2> renderables;
+	std::vector<Material*> materials;
+	std::vector<Mesh*> meshes;
+
+	std::vector<Handle<RenderObject2>> dirtyObjects;
+
+	std::array<MeshPass,8> meshPasses;
+
+	std::unordered_map<Material*, Handle<Material>> materialConvert;
+	std::unordered_map<Mesh*, Handle<Mesh>> meshConvert;
+
+	Handle<Material> getMaterialHandle(Material* m);
+	Handle<Mesh> getMeshHandle(Mesh* m);
+
+
+	AllocatedBuffer uploadBuffer[2];
+
+	AllocatedBuffer objectDataBuffer;
+	AllocatedBuffer indirectDrawBuffer;
+};
