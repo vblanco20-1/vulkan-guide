88d0d9e71fa19303a8a7907098f88cb950c3ae12
victor-blanco <1417000+vblanco20-1@users.noreply.github.com>
2023-12-23 16:24:33 +0100

    syncing initializers

--- src/vk_initializers.cpp
+++ src/vk_initializers.cpp
@@ -141,6 +141,7 @@ VkRenderingAttachmentInfo vkinit::attachment_info(
     return colorAttachment;
 }
 //< color_info
+//> depth_info
 VkRenderingAttachmentInfo vkinit::depth_attachment_info(
     VkImageView view, VkImageLayout layout /*= VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL*/)
 {
@@ -156,6 +157,7 @@ VkRenderingAttachmentInfo vkinit::depth_attachment_info(
 
     return depthAttachment;
 }
+//< depth_info
 //> render_info
 VkRenderingInfo vkinit::rendering_info(VkExtent2D renderExtent, VkRenderingAttachmentInfo* colorAttachment,
     VkRenderingAttachmentInfo* depthAttachment)
@@ -180,9 +182,9 @@ VkImageSubresourceRange vkinit::image_subresource_range(VkImageAspectFlags aspec
     VkImageSubresourceRange subImage {};
     subImage.aspectMask = aspectMask;
     subImage.baseMipLevel = 0;
-    subImage.levelCount = 1;
+    subImage.levelCount = VK_REMAINING_MIP_LEVELS;
     subImage.baseArrayLayer = 0;
-    subImage.layerCount = 1;
+    subImage.layerCount = VK_REMAINING_ARRAY_LAYERS;
 
     return subImage;
 }
@@ -331,102 +333,5 @@ VkPipelineShaderStageCreateInfo vkinit::pipeline_shader_stage_create_info(VkShad
     info.module = shaderModule;
     // the entry point of the shader
     info.pName = entry;
-    return info;
-}
-
-VkPipelineVertexInputStateCreateInfo vkinit::vertex_input_state_create_info()
-{
-    VkPipelineVertexInputStateCreateInfo info = {};
-    info.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
-    info.pNext = nullptr;
-
-    // no vertex bindings or attributes
-    info.vertexBindingDescriptionCount = 0;
-    info.vertexAttributeDescriptionCount = 0;
-    return info;
-}
-
-VkPipelineInputAssemblyStateCreateInfo vkinit::input_assembly_create_info(VkPrimitiveTopology topology)
-{
-    VkPipelineInputAssemblyStateCreateInfo info = {};
-    info.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
-    info.pNext = nullptr;
-
-    info.topology = topology;
-    // we are not going to use primitive restart on the entire tutorial so leave
-    // it on false
-    info.primitiveRestartEnable = VK_FALSE;
-    return info;
-}
-VkPipelineRasterizationStateCreateInfo vkinit::rasterization_state_create_info(VkPolygonMode polygonMode)
-{
-    VkPipelineRasterizationStateCreateInfo info = {};
-    info.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
-    info.pNext = nullptr;
-
-    info.depthClampEnable = VK_FALSE;
-    // rasterizer discard allows objects with holes, default to no
-    info.rasterizerDiscardEnable = VK_FALSE;
-
-    info.polygonMode = polygonMode;
-    info.lineWidth = 1.0f;
-    // no backface cull
-    info.cullMode = VK_CULL_MODE_NONE;
-    info.frontFace = VK_FRONT_FACE_CLOCKWISE;
-    // no depth bias
-    info.depthBiasEnable = VK_FALSE;
-    info.depthBiasConstantFactor = 0.0f;
-    info.depthBiasClamp = 0.0f;
-    info.depthBiasSlopeFactor = 0.0f;
-
-    return info;
-}
-VkPipelineMultisampleStateCreateInfo vkinit::multisampling_state_create_info()
-{
-    VkPipelineMultisampleStateCreateInfo info = {};
-    info.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
-    info.pNext = nullptr;
-
-    info.sampleShadingEnable = VK_FALSE;
-    // multisampling defaulted to no multisampling (1 sample per pixel)
-    info.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
-    info.minSampleShading = 1.0f;
-    info.pSampleMask = nullptr;
-    info.alphaToCoverageEnable = VK_FALSE;
-    info.alphaToOneEnable = VK_FALSE;
-    return info;
-}
-
-VkPipelineColorBlendAttachmentState vkinit::color_blend_attachment_state()
-{
-    VkPipelineColorBlendAttachmentState colorBlendAttachment = {};
-    colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
-    colorBlendAttachment.blendEnable = VK_FALSE;
-    return colorBlendAttachment;
-}
-VkPipelineRenderingCreateInfo vkinit::pipeline_render_info(VkFormat* colorFormat)
-{
-    VkPipelineRenderingCreateInfo info {};
-    info.sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO;
-    info.pNext = nullptr;
-
-    info.viewMask = 0;
-    info.colorAttachmentCount = 1;
-    info.pColorAttachmentFormats = colorFormat;
-    info.depthAttachmentFormat = VK_FORMAT_D32_SFLOAT;
-    info.stencilAttachmentFormat = VK_FORMAT_UNDEFINED;
-
-    return info;
-}
-
-VkPipelineDepthStencilStateCreateInfo vkinit::pipeline_depth_stencil_create_info()
-{
-    VkPipelineDepthStencilStateCreateInfo info {};
-    info.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
-    info.pNext = nullptr;
-
-    info.minDepthBounds = 0.f;
-    info.maxDepthBounds = 1.f;
-
     return info;
 }
